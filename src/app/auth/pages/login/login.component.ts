bad6037508de010bf241adb0ce4daa24d198b70f
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-04 15:31:32 -0300

    V393 - Login, interface, api

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -17,13 +18,22 @@ export class LoginComponent {
   }); 
 
   constructor( private fb: FormBuilder,
-               private router: Router) { }
+               private router: Router,
+               private authService: AuthService) { }
  
   login(){
     console.log(this.myFormLogin.value);
     console.log(this.myFormLogin.valid);
 
-    this.router.navigateByUrl('/dashboard')
+    const {email, password} = this.myFormLogin.value;
+
+    this.authService.login(email, password)
+        .subscribe( resp =>{
+          console.log(resp);
+          
+        })
+
+    //this.router.navigateByUrl('/dashboard')
   }
 
 }
